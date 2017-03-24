04044b5974e57c2340d0f758a3eaa6a5483fe0ee
matt d lee <mattdhlee@gmail.com>
2017-03-23 20:48:23 -0400

    Starting merge sort implementation project

---
+++ src/MergeSort.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Matt on 2017-03-23.
+ */
+public class MergeSort {
+
+
+    static int simpleMerge() {
+        //TODO: implement simple recursive mergesort from the book
+        return -1;
+    }
+
+
+    
+}
